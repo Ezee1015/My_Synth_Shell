5b211cac34bdd92bfadcd35e57d9db6640623b99
Ezee1015 <routerciscoanuitti@gmail.com>
2022-01-11 12:44:28 -0300

    Agrega a Manjaro

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,4 @@
+cd Customizacion
 sudo apt install fonts-powerline
 git clone --recursive https://github.com/andresgongora/synth-shell.git
 chmod +x synth-shell/setup.sh
@@ -7,6 +8,7 @@ echo "aprete ’i’,’u’,’n’,’y’,’n’,’n’,’n’"
 ./setup.sh
 cd ..
 cp Nombre_Distro.txt ~/.Nombre_Distro.txt
+cp Nombre_Distro_Manjaro.txt ~/.Nombre_Distro_Manjaro.txt
 cp synth-shell-prompt.config ~/.config/synth-shell/synth-shell-prompt.config
 
 echo "clear" >> ~/.bashrc
@@ -24,3 +26,26 @@ echo "# neofetch -L" >> ~/.bashrc
 echo "" >> ~/.bashrc
 echo "alias actualizar='sudo apt update && sudo apt upgrade -y && sudo apt autoremove --purge -y && flatpak update'" >> ~/.bashrc
 echo "alias ifconfig='nmcli'" >> ~/.bashrc
+
+
+
+echo "PRUEBA=$(distro | grep Manjaro)" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "if [ "$PRUEBA" == "" ] ; then" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "    NEODEBIAN=$(cat ~/.Nombre_Distro.txt)" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "    RED='\033[1;31m'" >> ~/.bashrc
+echo "    BLUE='\033[1;34m'" >> ~/.bashrc
+echo "    NC='\033[0m' # No Color" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "    # echo -e "${RED} $NEODEBIAN \n"" >> ~/.bashrc
+echo "    echo -e "${BLUE} $NEODEBIAN \n"" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "else" >> ~/.bashrc
+echo "    NEOMANJARO=$(cat ~/.Nombre_Distro_Manjaro.txt)" >> ~/.bashrc
+echo "    echo -e "$NEOMANJARO"" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "    alias actualizar='sudo apt update && sudo apt upgrade -y && sudo apt autoremove --purge -y && flatpak update'" >> ~/.bashrc
+echo "    alias ifconfig='nmcli'" >> ~/.bashrc
